1b52b3ea3df675d0863f4738bc2a823c6c8bbb50
Allen <allen.s.kim2001@gmail.com>
2025-04-18 16:13:11 -0400

    enemy chase AI finally working

--- FinalGame/Assets/Enemy.cs
+++ FinalGame/Assets/Enemy.cs
@@ -5,6 +5,13 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform player;
+    public float playerDistance;
+    public float awareAI = 15f;
+    public float AIMoveSpeed;
+    //bool isChasing = false;
+
+
     [SerializeField] float waitTimeOnWaypoint = 1f;
     [SerializeField] Path path;
 
@@ -15,33 +22,78 @@ public class Enemy : MonoBehaviour
 
     float enemyHealth = 3;
 
+    public int thisEnemyType;
+
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         //Animator = GetComponent<Animator>();
+
+        agent.speed = 3.5f;
     }
 
     // Start is called before the first frame update
     private void Start()
     {
         agent.destination = path.GetCurrentWaypoint();
+        agent.autoBraking = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if(agent.remainingDistance <= 0.1f)
+        // if the enemy is either a Chaser or Attacker, calculate their
+        // distance from the player and chase accordingly!!
+
+        if (thisEnemyType > 1)
+        {
+            playerDistance = Vector3.Distance(player.position, transform.position);
+
+            //Debug.Log(playerDistance);
+
+            if (playerDistance < awareAI)
+            {
+                LookAtPlayer();
+
+                if (playerDistance > 0.1f)
+                {
+                    agent.speed = AIMoveSpeed;
+                    Chase();
+                }
+                else
+                {
+                    agent.destination = path.GetNextWaypoint();
+                }
+            }
+
+        }
+
+        if (agent.remainingDistance < 0.1f)
         {
             time += Time.deltaTime;
-            if(time >= waitTimeOnWaypoint)
+            if (time >= waitTimeOnWaypoint)
             {
                 time = 0f;
                 agent.destination = path.GetNextWaypoint();
             }
         }
 
+
         // for animation stuff
         //float normalizedSpeed = Mathf.InverseLerp(0f, agent.speed, agent.velocity.magnitude);
+    }
+
+    void LookAtPlayer()
+    {
+        transform.LookAt(player);
+    }
+
+    void Chase()
+    {
+        transform.Translate(Vector3.forward * AIMoveSpeed * Time.deltaTime);
+        agent.SetDestination(player.position);
+        //agent.destination = player.position;
     }
 
     public void TakeDamage(float damagePoints)
